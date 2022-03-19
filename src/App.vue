6200fa7b141dd0e40ee89e02f10e25e5152955bd
lleeeuuuuanchik <101695866+lleeeuuuuanchik@users.noreply.github.com>
2022-03-19 11:20:17 +0300

    1.0

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
+
+
+// ------------------------------------------------------ HTML
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <div class = "slider">
+    <carousel :items-to-show="1" :autoplay="10000" :transition="1500" :wrap-around="true" :touchDrag="false" :mouseDrag="false">
+      <slide v-for="(slide, index) in arraySlider" :key="index">
+        <img :src="require(`./assets/img/for_slide/${slide.img}`)" class="slider__img" alt="">
+      </slide>
+    </carousel>
+    <div class="slider__header">
+      <img src="./assets/img/logo_white.png" class="header__logo" alt="">
+    </div>
+  </div>
+  
+</template>_
+
+
+// ------------------------------------------------------ JS
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import database from "./array.js"
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide } from 'vue3-carousel';
 export default {
-  name: 'App',
+  data() {
+    return {
+      arraySlider: []
+    }
+  },
+
   components: {
-    HelloWorld
+    Carousel,
+    Slide,
+  },
+
+  created() {
+    this.arraySlider = database.sliderImg()
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+// ------------------------------------------------------ CSS
+
+<style src="./assets/perfomance.css" >
+
 </style>
